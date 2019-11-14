1924b8c23668ba17f0a0d4373f7c9a3cb7e6c05a
Nicolás Quiroz <nh.quiroz@gmail.com>
2019-11-14 05:33:10 -0300

    fix assets url

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,9 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano')({
-  preset: 'default'
-});
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./dist/index.html']
 });
 
 module.exports = {
-  plugins: [tailwindcss, autoprefixer, purgecss, cssnano]
+  plugins: [tailwindcss, autoprefixer, purgecss]
 };
